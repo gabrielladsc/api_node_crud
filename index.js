37fe11299fe7682c7c1d76baa36919cb1bd106b2
Gabriella da Silva Correia <gabriella.correa@toodoo.com.br>
2022-05-02 14:22:39 -0300

    initial commit

---
+++ index.js
@@ -0,0 +1,37 @@
+//configuração inicial
+require('dotenv').config()
+const express = require('express')
+const mongoose = require('mongoose')
+const app = express()
+
+// forma de ler JSON
+app.use(
+    express.urlencoded({
+        extended: true
+    })
+)
+
+app.use(express.json())
+
+const personRoutes = require('./routes/personRoutes')
+
+app.use('/person', personRoutes)
+
+// rota inicial (endpoint)
+app.get('/', (req, res) => {
+    res.json({ message: 'Teste!' })
+})
+
+//103055!!
+//mongodb+srv://Gabriella:103055!!@apicluster.vbzx0.mongodb.net/bancodaapi?retryWrites=true&w=majority
+
+const DB_USER = process.env.DB_USER
+const DB_PASSWORD = process.env.DB_PASSWORD
+
+//entregar uma porta
+mongoose.connect(`mongodb+srv://${DB_USER}:${DB_PASSWORD}@apicluster.vbzx0.mongodb.net/bancodaapi?retryWrites=true&w=majority`)
+    .then(() => {
+        app.listen(3000)
+        console.log('Conectado ao MongoDB')
+    })
+    .catch((err) => console.log(err))
